aeefe18c85bd0055e19260f188ae5a4728f4a96d
Thiago Arthur <thiagodevprof@gmail.com>
2025-01-30 21:49:46 -0300

    Reducers Update 2.0

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -3,7 +3,16 @@ import './ProductList.css'
 import CartItem from './CartItem';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
-    const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [showPlants, setShowPlants] = useState(false);
+    const [addedCart, setAddedCart] = useState({});
+
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+           ...prevState,
+           [product.name]: true,
+         }));
+      };
 
     const plantsArray = [
         {
@@ -268,7 +277,20 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
-
+            {plantsArray.map((category, index) => (
+    <div key={index}>
+        <h1 style={{}}><div style={{alignContent: 'center', textAlign: 'center', marginTop: '23px'}}>{category.category}</div></h1>
+        <div className="product-list">
+            {category.plants.map((plant, plantIndex) => (
+            <div className="product-card" key={plantIndex}>
+                <img className="product-image" src={plant.image} alt={plant.name} />
+                <div className="product-title">{plant.name}</div>
+                <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+            </div>
+            ))}
+        </div>
+    </div>
+    ))}
 
         </div>
  ) :  (
